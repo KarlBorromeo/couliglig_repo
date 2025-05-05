97da2cd1e11236ebee874927c4623fa84878c57c
Karl Borromeo <lrakborromeo@gmail.com>
2025-05-05 23:07:10 +0800

    working mapping for single robot

--- src/couliglig_bot/launch/robot_launch.py
+++ src/couliglig_bot/launch/robot_launch.py
@@ -8,14 +8,13 @@ from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription
 from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory, get_packages_with_prefixes
 import launch
 
 package_dir = get_package_share_directory('couliglig_bot')
-turtlebot3_multi_robot_dir = get_package_share_directory('turtlebot3_multi_robot')
 
 def fix_mesh_path_error():
     wbt_path = os.path.join(package_dir,'worlds','couliglig_bot.wbt')
@@ -30,63 +29,105 @@ def fix_mesh_path_error():
 
 
 def generate_launch_description():
+    world = LaunchConfiguration('world', default='couliglig_bot.wbt')
+    mode = LaunchConfiguration('mode',default='realtime')
     use_nav = LaunchConfiguration('nav', default=False)
     use_slam = LaunchConfiguration('slam', default=False)
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-    namespace = 'robot1'
     # fix the mesh path error of wbt
     fix_mesh_path_error()
 
-    ld = LaunchDescription()
-    ros2_control_params = os.path.join(package_dir, 'config', 'ros2control.yaml')
-    mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'), ('/diffdrive_controller/odom', '/odom')]
 
-    # Remappings TODO:
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+    # launch the instance of the webots
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir,'worlds','couliglig_bot.wbt'),
+        mode='realtime',
+        ros2_supervisor=True
+    )
 
-    urdf = os.path.join(
-        package_dir, 'resource', 'couliglig_bot.urdf'
-    )   
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        namespace=namespace,
         output='screen',
         parameters=[{
-            'robot_description': '<robot name=""><link name=""/></robot>',
+            'robot_description': '<robot name=""><link name=""/></robot>'
         }],
-        remappings=remappings,
     )
+
     footprint_publisher = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        namespace=namespace,
         output='screen',
-        arguments=['0', '0', '0', '0', '0', '0','base_footprint','base_link'],
-        remappings=remappings,
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint'],
     )
 
-    # Create spawn call
+
+    # ROS control spawners
+    # controller_manager_timeout = ['--controller-manager-timeout', '50']
+    # controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
+    # diffdrive_controller_spawner = Node(
+    #     package='controller_manager',
+    #     executable='spawner',
+    #     output='screen',
+    #     prefix=controller_manager_prefix,
+    #     arguments=['diffdrive_controller'] + controller_manager_timeout,
+    # )
+    # joint_state_broadcaster_spawner = Node(
+    #     package='controller_manager',
+    #     executable='spawner',
+    #     output='screen',
+    #     prefix=controller_manager_prefix,
+    #     arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+    # )
+    # ros_control_spawners = [diffdrive_controller_spawner, joint_state_broadcaster_spawner]
+
+    robot_description_path = os.path.join(package_dir, 'resource', 'couliglig_bot.urdf')
+    ros2_control_params = os.path.join(package_dir, 'config', 'ros2control.yaml')
+    mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'), ('/diffdrive_controller/odom', '/odom')]
+    
     turtlebot_driver = WebotsController(
         robot_name='robot1',
-        namespace=namespace,
         parameters=[
-            {'robot_description': urdf,
+            {'robot_description': robot_description_path,
              'use_sim_time': use_sim_time,
-             'set_robot_state_publisher': True,
-             'update_rate': 50,  
-             },
-            #  ros2_control_params
+             'update_rate': 50, 
+             'set_robot_state_publisher': True},
+            # ros2_control_params
         ],
-        remappings=[*remappings],
+        remappings=mappings,
         respawn=True
     )
 
-    # ld.add_action(turtlebot_state_publisher)
-    ld.add_action(robot_state_publisher)
-    ld.add_action(footprint_publisher)
-    ld.add_action(turtlebot_driver)
+    # Navigation
+    navigation_nodes = []
+    
+    # SLAM
+    if 'turtlebot3_cartographer' in get_packages_with_prefixes():
+        turtlebot_slam = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory('turtlebot3_cartographer'), 'launch', 'cartographer.launch.py')),
+            launch_arguments=[
+                ('use_sim_time', use_sim_time),
+            ],
+            condition=launch.conditions.IfCondition(use_slam))
+        navigation_nodes.append(turtlebot_slam)
 
-    return ld
+    # Wait for the simulation to be ready to start navigation nodes
+    waiting_nodes = WaitForControllerConnection(
+        target_driver=turtlebot_driver,
+        nodes_to_start=navigation_nodes
+    )
+
+    return LaunchDescription([
+
+        webots,
+        webots._supervisor,
+
+        robot_state_publisher,
+        footprint_publisher,
+        
+        turtlebot_driver,
+        waiting_nodes
+    ])
+
 
